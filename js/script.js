9ac9ecc7f7d50e21691df67fe5ea0bb33d4d8017
lapael1 <lapael6@gmail.com>
2024-07-04 12:03:12 +0900

    모달 창 만들기, 클릭 시 띄우기, 닫기 이미지 슬라이드 넘어가는 기능 navi에 해당하는 submenu/전체가 나오는 코드

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
-$(document).ready(function(){
-            // tapmenu
-            $(function(){
-                $(".tabmenu > li> a").click(function(){
-                    $(this).parent().addClass("active").siblings().removeClass("active")
-                })
-            })
+$(document).ready(function () {
+  // $(".navi > li").mouseover(function(){
+  //     // 서브메뉴가 전체가 보여지는 코드
+  //     $(".submenu").stop().slideDown(500)
+  // })
+  // .mouseout(function(){
+  //     $(".submenu").stop().slideUp(500)
+  // })
+
+  // navi에 해당하는 submenu가 나오는 코드
+  $(".navi > li").mouseover(function () {
+    // console.log(this)
+    $(this).find(".submenu").stop().slideDown(500);
+  });
+
+  $(".navi > li").mouseout(function () {
+    $(this).find(".submenu").stop().slideUp(500);
+  });
+  // 이미지 슬라이드 기능
+  setInterval(function () {
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -1200 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: -2400 });
+    $(".slidelist").delay(2000);
+    $(".slidelist").animate({ marginLeft: 0 });
+    $(".slidelist").delay(2000);
+  });
+  // modal기능
+  $(".popup").click(function () {
+    $(".modal").addClass("active");
+  });
+// 닫기 기능
+$(".close").click(function(){
+    $(".modal").removeClass("active")
 })
+  // tapmenu
+  $(function () {
+    $(".tabmenu > li> a").click(function () {
+      $(this).parent().addClass("active").siblings().removeClass("active");
+    });
+  });
+});
